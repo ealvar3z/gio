0d66d67da1fc657ccdb775f029917d047619ae8e
ealvar3z <55966724+ealvar3z@users.noreply.github.com>
2025-02-08 23:18:56 -0500

    fix spelling errors

--- gio.go
+++ gio.go
@@ -7,11 +7,14 @@ import (
 	"strings"
 )
 
-// Command embeds an exec.Cmd and also includes buffers for std{in,err,out}
-// These will automatically attached when they're instantiated.
+// Command embeds an exec.Cmd and also includes buffers for
+// std{in,out,err} These will automatically attach when they're
+// instantiated.
 type Command struct {
 	*exec.Cmd
-	BioStdin, BioStdout, BioStderr *bytes.Buffer // Bio from Plan9's buffer io (Bio)
+	BioStdin  *bytes.Buffer // buffer io from Plan9 (Bio)
+	BioStdout *bytes.Buffer
+	BioStderr *bytes.Buffer 
 }
 
 // Satisfies the interface
@@ -19,9 +22,9 @@ func (c *Command) String() string {
 	return strings.Join(c.Args, " ")
 }
 
-// Constructor: i.e. instantiates a new pointer to a Command struct and attaches the command's
-// std{in,out,err} file descriptors
-func New(name string, arg ..string) *Command {
+// New instantiates a new pointer to a Command struct and
+// attaches the command's std{in,out,err} file descriptors
+func New(name string, arg ...string) *Command {
 	stdin := new(bytes.Buffer)
 	stdout := new(bytes.Buffer)
 	stderr := new(bytes.Buffer)
@@ -29,24 +32,28 @@ func New(name string, arg ..string) *Command {
 	cmd := exec.Command(name, arg...)
 	cmd.Stdin := stdin
 	cmd.Stdout := stdout
-	cmd.Stderr:= stderr
+	cmd.Stderr := stderr
 
 	return &Command{
-		Cmd:		cmd,
-		BioStdin:	stdin,
-		BioStdout:	stdout,
-		BioStderr:	stderr,
+		Cmd:       cmd,
+		BioStdin:  stdin,
+		BioStdout: stdout,
+		BioStderr: stderr,
 	}
 }
 
-// Run calls `*exec.Cmd.Run()` as a wrapper to Command and returns
-// an error. If `Run()` fails, Run() will return the error also by
-// checking stderr's buffer.
+// Run calls `*exec.Cmd.Run()` as a wrapper to Command and returns an
+// error. If `Run()` fails, Run() will return the error also by checking
+// stderr's buffer.
 //
 // If stderr's buffer is empty, an error was returned with its contents.
 func (c *Command) Run() error {
-	if err := c.Start(); err != nil { return fmt.Errorf("[ERROR]: cmd %s failed with: %v", c, err) }
-	if err := c.Wait(); err != nil { return err }
+	if err := c.Start(); err != nil {
+		return fmt.Errorf("[ERROR]: cmd %s failed with: %v", c, err)
+	}
+	if err := c.Wait(); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -55,7 +62,7 @@ func (c *Command) Run() error {
 func (c *Command) Wait() error {
 	if err := c.Cmd.Wait(); err != nil {
 		if c.BioStderr.Len() > 0 {
-			return fmt.Errorf("[ERROR]: cmd %s: failes with %s.\n\n here:%s",
+			return fmt.Errorf("[ERROR]: %s: failed with %s.\n\n here:%s",
 				c, err, c.BioStderr.String())
 		}
 		return fmt.Errorf("[ERROR]: running %s failed with: %s.", c, err)
